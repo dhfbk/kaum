d635a9fb812cbdb751004c7c9a5edad45ced743c
Alessio Palmero Aprosio <alessio@apnetwork.it>
2022-10-24 18:13:10 +0200

    Work in progress

--- api/tasks/rc/_common.php
+++ api/tasks/rc/_common.php
@@ -10,86 +10,101 @@ if (!$result) {
     dieWithError($error);
 }
 
-function rc_confirmTask($TaskID, &$Info) {
+function rc_confirmTask($TaskID, &$Info, &$ret) {
 
-    $group = null;
-    $groupName = "t" . $TaskID . "-" . $Info['type_info']['channel_name'];
+    return "Generic error";
 
-    try {
-        $group = new \ATDev\RocketChat\Groups\Group();
-        $group->setName($groupName);
-        $result = $group->create();
-        $Info['type_info']['channel_id'] = $group->getGroupId();
-        $ret['log'][] = date("H:i:s")." - Group {$groupName} created successfully";
-    } catch (Exception $e) {
-        // $ret['log'][] = "Error in creating group {$groupName}: " . $e->getMessage();
-        return $e->getMessage();
+    if (isset($Info['type_info']['rc_groups']) && $Info['type_info']['rc_groups'] > 1) {
+        $groupNames = [];
+
+        if ($Info['type_info']['rc_uniqueScenario']) {
+            for ($i = 1; $i <= $Info['type_info']['rc_groups']; $i++) {
+                $groupNames[] = "t" . $TaskID . "-" . $Info['type_info']['channel_name'] . "-" . $i;
+            }
+        }
+
+        // to do
     }
+    else {
+        $group = null;
+        $groupName = "t" . $TaskID . "-" . $Info['type_info']['channel_name'];
 
-    if ($Info['type_info']['teacher_can_join']) {
-        $query = "SELECT * FROM users
-            WHERE project = '{$ProjectID}'
-                AND educator = '1'
-                AND deleted = '0'";
+        try {
+            $group = new \ATDev\RocketChat\Groups\Group();
+            $group->setName($groupName);
+            $result = $group->create();
+            $Info['type_info']['channel_id'] = $group->getGroupId();
+            $ret['log'][] = date("H:i:s")." - Group {$groupName} created successfully";
+        } catch (Exception $e) {
+            // $ret['log'][] = "Error in creating group {$groupName}: " . $e->getMessage();
+            return $e->getMessage();
+        }
+
+        if ($Info['type_info']['teacher_can_join']) {
+            $query = "SELECT * FROM users
+                WHERE project = '{$ProjectID}'
+                    AND educator = '1'
+                    AND deleted = '0'";
+            $result = $mysqli->query($query);
+            while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                $user = new \ATDev\RocketChat\Users\User($row['username']);
+
+                // This needs to be here
+                // TODO: try/catch
+                $user->info();
+                $group->invite($user);
+                // $ret['log'][] = "User {$row['username']} added to group {$groupName}";
+            }
+        }
+
+        $query = "SELECT * FROM users WHERE task = '$TaskID'";
         $result = $mysqli->query($query);
         while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-            $user = new \ATDev\RocketChat\Users\User($row['username']);
+            try {
+                $user = new \ATDev\RocketChat\Users\User();
+                $user->setName($row['username']);
+                $user->setEmail($row['username'] . "@kidactions.eu");
+                $user->setUsername($row['username']);
+                $user->setPassword($row['password']);
 
-            // This needs to be here
-            // TODO: try/catch
-            $user->info();
-            $group->invite($user);
-            // $ret['log'][] = "User {$row['username']} added to group {$groupName}";
-        }
-    }
-
-    $query = "SELECT * FROM users WHERE task = '$TaskID'";
-    $result = $mysqli->query($query);
-    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-        try {
-            $user = new \ATDev\RocketChat\Users\User();
-            $user->setName($row['username']);
-            $user->setEmail($row['username'] . "@kidactions.eu");
-            $user->setUsername($row['username']);
-            $user->setPassword($row['password']);
-
-            $user->create();
-            $ret['log'][] = date("H:i:s")." - User {$row['username']} created successfully";
-            $group->invite($user);
-            $ret['log'][] = date("H:i:s")." - User {$row['username']} added to group {$groupName}";
-            $r = $user->setActiveStatus(false);
-            if ($r) {
-                $ret['log'][] = date("H:i:s")." - User {$row['username']} deactivated";
+                $user->create();
+                $ret['log'][] = date("H:i:s")." - User {$row['username']} created successfully";
+                $group->invite($user);
+                $ret['log'][] = date("H:i:s")." - User {$row['username']} added to group {$groupName}";
+                $r = $user->setActiveStatus(false);
+                if ($r) {
+                    $ret['log'][] = date("H:i:s")." - User {$row['username']} deactivated";
+                }
+                // else {
+                //     $ret['log'][] = "Error in deactivating {$row['username']}: " . $user->getError();
+                // }
+            } catch (Exception $e) {
+                // echo $e->getMessage();
+                // $ret['log'][] = "Error in creating user {$row['username']}: " . $e->getMessage();
             }
-            // else {
-            //     $ret['log'][] = "Error in deactivating {$row['username']}: " . $user->getError();
-            // }
-        } catch (Exception $e) {
-            // echo $e->getMessage();
-            // $ret['log'][] = "Error in creating user {$row['username']}: " . $e->getMessage();
         }
-    }
 
-    $message = new \ATDev\RocketChat\Messages\Message();
-    $message->setRoomId($groupName);
-    // $message->setAlias("Kid Actions Admin");
-    // $message->setEmoji(":house:");
-    $message->setText($Info['type_info']['description']);
-    $result = $message->postMessage();
-
-    if (!$Info['type_info']['teacher_can_join']) {
         $message = new \ATDev\RocketChat\Messages\Message();
         $message->setRoomId($groupName);
         // $message->setAlias("Kid Actions Admin");
-        $message->setEmoji(":sos:");
-        $message->setText("Educators cannot join this conversation. If you send */sos* (slash + sos) in the chat, the educators are notified and are allowed to the chat. Other users are notified that someone asks for help, but the caller's identity is not revealed.");
+        // $message->setEmoji(":house:");
+        $message->setText($Info['type_info']['description']);
         $result = $message->postMessage();
-        $ret['log'][] = date("H:i:s")." - Initial message sent";
-    }
 
-    $dataJson = addslashes(json_encode($Info));
-    $query = "UPDATE tasks SET data = '$dataJson' WHERE id = '{$TaskID}'";
-    $mysqli->query($query);
+        if (!$Info['type_info']['teacher_can_join']) {
+            $message = new \ATDev\RocketChat\Messages\Message();
+            $message->setRoomId($groupName);
+            // $message->setAlias("Kid Actions Admin");
+            $message->setEmoji(":sos:");
+            $message->setText("Educators cannot join this conversation. If you send */sos* (slash + sos) in the chat, the educators are notified and are allowed to the chat. Other users are notified that someone asks for help, but the caller's identity is not revealed.");
+            $result = $message->postMessage();
+            $ret['log'][] = date("H:i:s")." - Initial message sent";
+        }
+
+        $dataJson = addslashes(json_encode($Info));
+        $query = "UPDATE tasks SET data = '$dataJson' WHERE id = '{$TaskID}'";
+        $mysqli->query($query);
+    }
 }
 
 function rc_channelNameIsWrong($channelName) {
--- api/tasks/rc/_confirmTask.php
+++ api/tasks/rc/_confirmTask.php
@@ -10,83 +10,9 @@ if (!$TaskID) {
 */
 
 $ret['log'] = [];
-$group = null;
-$groupName = "t" . $TaskID . "-" . $Info['type_info']['channel_name'];
 
-dieWithError(print_r($Info, true));
-
-try {
-    $group = new \ATDev\RocketChat\Groups\Group();
-    $group->setName($groupName);
-    $result = $group->create();
-    $Info['type_info']['channel_id'] = $group->getGroupId();
-    $ret['log'][] = date("H:i:s")." - Group {$groupName} created successfully";
-} catch (Exception $e) {
-    // $ret['log'][] = "Error in creating group {$groupName}: " . $e->getMessage();
-    dieWithError($e->getMessage());
+$DeleteError = rc_confirmTask($TaskID, $Info, $ret);
+if ($DeleteError) {
+    $DeleteTask = true;
 }
 
-if ($Info['type_info']['teacher_can_join']) {
-    $query = "SELECT * FROM users
-        WHERE project = '{$ProjectID}'
-            AND educator = '1'
-            AND deleted = '0'";
-    $result = $mysqli->query($query);
-    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-        $user = new \ATDev\RocketChat\Users\User($row['username']);
-
-        // This needs to be here
-        // TODO: try/catch
-        $user->info();
-        $group->invite($user);
-        // $ret['log'][] = "User {$row['username']} added to group {$groupName}";
-    }
-}
-
-$query = "SELECT * FROM users WHERE task = '$TaskID'";
-$result = $mysqli->query($query);
-while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-    try {
-        $user = new \ATDev\RocketChat\Users\User();
-        $user->setName($row['username']);
-        $user->setEmail($row['username'] . "@kidactions.eu");
-        $user->setUsername($row['username']);
-        $user->setPassword($row['password']);
-
-        $user->create();
-        $ret['log'][] = date("H:i:s")." - User {$row['username']} created successfully";
-        $group->invite($user);
-        $ret['log'][] = date("H:i:s")." - User {$row['username']} added to group {$groupName}";
-        $r = $user->setActiveStatus(false);
-        if ($r) {
-            $ret['log'][] = date("H:i:s")." - User {$row['username']} deactivated";
-        }
-        // else {
-        //     $ret['log'][] = "Error in deactivating {$row['username']}: " . $user->getError();
-        // }
-    } catch (Exception $e) {
-        // echo $e->getMessage();
-        // $ret['log'][] = "Error in creating user {$row['username']}: " . $e->getMessage();
-    }
-}
-
-$message = new \ATDev\RocketChat\Messages\Message();
-$message->setRoomId($groupName);
-// $message->setAlias("Kid Actions Admin");
-// $message->setEmoji(":house:");
-$message->setText($Info['type_info']['description']);
-$result = $message->postMessage();
-
-if (!$Info['type_info']['teacher_can_join']) {
-    $message = new \ATDev\RocketChat\Messages\Message();
-    $message->setRoomId($groupName);
-    // $message->setAlias("Kid Actions Admin");
-    $message->setEmoji(":sos:");
-    $message->setText("Educators cannot join this conversation. If you send */sos* (slash + sos) in the chat, the educators are notified and are allowed to the chat. Other users are notified that someone asks for help, but the caller's identity is not revealed.");
-    $result = $message->postMessage();
-    $ret['log'][] = date("H:i:s")." - Initial message sent";
-}
-
-$dataJson = addslashes(json_encode($Info));
-$query = "UPDATE tasks SET data = '$dataJson' WHERE id = '{$TaskID}'";
-$mysqli->query($query);
