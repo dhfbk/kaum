8ed075a5c3e1e39843d09b1f176ea8f35efc1a7d
Alessio Palmero Aprosio <alessio@apnetwork.it>
2022-10-26 14:56:09 +0200

    Change username fix in Rocket.Chat

--- api/tasks/rc/_changeUserName.php
+++ api/tasks/rc/_changeUserName.php
@@ -14,11 +14,13 @@ if ($Field == "name") {
     }
     $user = new \ATDev\RocketChat\Users\User($Username);
     $user->info();
-    $user->setName($NewValue);
-    $result = $user->update();
-    if (!$result) {
-        $error = $user->getError();
-        dieWithError($error);
+    if ($user->getUsername()) {
+        $user->setName($NewValue);
+        $result = $user->update();
+        if (!$result) {
+            $error = $user->getError();
+            dieWithError($error);
+        }
     }
 }
 
